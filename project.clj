b3ac5a371015e2a40549f27564cd1967263f3085
Jenel Myers <jenelmyers@Jenels-MacBook-Pro.local>
2020-08-27 21:44:51 -0700

    first commit

--- project.clj
+++ project.clj
@@ -13,7 +13,9 @@
                  [ring.middleware.logger "0.5.0"]
                  [compojure "1.5.0"]
                  [environ "1.0.3"]
-                 [reagent "0.6.0-rc"]]
+                 [reagent "0.6.0-rc"]
+                 [org.clojure/core.rrb-vector "0.0.14"]
+                 [javax.xml.bind/jaxb-api "2.2.4"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]
             [lein-environ "1.0.3"]]
--- src/cljs/kanban/core.cljs
+++ src/cljs/kanban/core.cljs
@@ -1,11 +1,55 @@
+(defn Card [card]
+  (if (:editing card)
+    [:div.card.editing [:input {:type "text" :value (:title card)}]]
+    [:div.card (:title card)]))
+
+(defn NewCard []
+  [:div.new-card
+   "+ add new card"])
+
+(defn Column [{:keys [title cards editing]}]
+  [:div.column
+   (if editing
+     [:input {:type "text" :value title}]
+     [:h2 title])
+   (for [c cards]
+     [Card c])
+   [NewCard]])
+
+(defn NewColumn []
+  [:div.new-column
+   "+ add new column"])
+
+(defn Board [state]
+  [:div.board
+   (for [c (:columns @state)]
+   [Column])
+   [NewColumn]])
+
+(def app-state
+  (r/atom {:columns
+           [{:title "Todos"
+             :cards [{:title "Learn about Reagent"}
+                     {:title "Tell my friends about Lambda Island"}]}]}))
+
 (ns kanban.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as r]))
+
+;;(defn set-title! [card title]
+;;(swap! card assoc :title title))
+
+;; same as
+(defn set-title! [card title]
+  (swap! card (fn [c] (assoc c :title title))))
+
+(get-in @board [:columns 1 :cards 0 :title]) ;;=> "Hello Turtles."
+(swap! board assoc-in [:columns 1 :cards 0 :title] "Hello Turtles.")
+
+
+(set-title! card-atom "Hello Turtles")
+
+card-atom
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
-
-(defn greeting []
-  [:h1 (:text @app-state)])
-
-(reagent/render [greeting] (js/document.getElementById "app"))
+(r/render [Board app-state] (js/document.getElementById "app"))
